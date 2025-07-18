590d68049d4203fcf03872a2a5b1426162b674cf
hailia sommerville <hailiassommerville@gmail.com>
2025-07-18 14:33:16 -0400

    chore: view poll page created

---
+++ src/pages/ViewPoll.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ViewPoll = () => {
+    return (
+        <div className="view-poll-page">
+            <h2>This is the poll view page</h2>
+        </div>
+    );
+};
