61d38e7856470fae068a979e19b97d923ae66513
hailia sommerville <hailiassommerville@gmail.com>
2025-07-18 15:09:08 -0400

    feat: slug extracted from url + basic poll info displayed

--- src/pages/ViewPoll.jsx
+++ src/pages/ViewPoll.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ViewPoll = () => {
-    return (
-        <div className="view-poll-page">
-            <h2>This is the poll view page</h2>
-        </div>
-    );
+    navigate = useNavigate();
+    const {slug} = useParams();
+    const [poll, setPoll] = useState(null);
+
+    useEffect(() => {
+        const fetchPoll = async () => {
+            try {
+                const res = await axios.get(`http://localhost:8080/api/polls/${slug}`);
+                setPoll(response.data);
+            } catch (error) {
+                console.error("Error fetching poll:", error);
+                navigate("*");
+            }
+        };
+
+        fetchPoll();
+    }, [slug]);
+
+
+
+  return (
+    <div className="view-poll-page">
+      <h2>{poll.title}</h2>
+      <p>{poll.deadline}</p>
+      <p>{poll.description}</p>
+    </div>
+  );
 };
+
+export default ViewPoll;
