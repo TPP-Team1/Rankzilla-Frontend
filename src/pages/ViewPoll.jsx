a84c7b44443410d6c1fa8e03ae68ad76bc522784
hailia sommerville <hailiassommerville@gmail.com>
2025-07-18 15:38:02 -0400

    feat: loading and error states added

--- src/pages/ViewPoll.jsx
+++ src/pages/ViewPoll.jsx
@@ -4,30 +4,45 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const ViewPoll = () => {
-    navigate = useNavigate();
+    const navigate = useNavigate();
     const {slug} = useParams();
     const [poll, setPoll] = useState(null);
     const [isExpired, setIsExpired] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         const fetchPoll = async () => {
             try {
                 const res = await axios.get(`http://localhost:8080/api/polls/${slug}`);
-                setPoll(response.data);
+                setPoll(res.data);
             } catch (error) {
-                console.error("Error fetching poll:", error);
+                setError("Failed to fetch poll data.");
+                console.error(error);
                 navigate("*");
+            }
+            finally {
+                setLoading(false);
             }
         };
 
         fetchPoll();
     }, [slug]);
 
-    handleExpired = () => {
+    const handleExpired = () => {
         const now = new Date();
         const deadline = new Date(poll.deadline);
         setIsExpired(now > deadline);
     }
+
+    useEffect(() => {
+        if (poll) {
+            handleExpired();
+        }
+    }, [poll]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>{error}</p>;
 
   return (
     <div className="view-poll-page">
